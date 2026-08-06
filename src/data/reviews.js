6a7ffd14478944f40aa1653e3ad4a74c95e980d9
toji-t <mahadmk1@gmail.com>
2026-08-06 14:29:31 +0500

    Update reviews.js

--- src/data/reviews.js
+++ src/data/reviews.js
@@ -143,7 +143,7 @@ export const reviews = [
 },
 {
    slug: "apple-watch-series-11"
-  productSlug: "Apple Watch Series 11 [GPS 42mm] Smartwatch - Jet Black Aluminum Case with Black Sport Band",
+  productSlug: "apple-watch-series-11"
   title: "Apple Watch Series 11 Review: Is Apple's Best Smartwatch Worth Buying in 2026?",
 
   excerpt:
