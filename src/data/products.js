e6d4d509f75cfc4efc6e13b550a5d509bd310afe
toji-t <mahadmk1@gmail.com>
2026-08-06 14:26:12 +0500

    Update products.js

--- src/data/products.js
+++ src/data/products.js
@@ -154,7 +154,7 @@ affiliateUrl: "https://www.amazon.com/dp/B0GX78S7QQ?tag=gearnest11-20",
 },
 
 {
- slug: "apple-watch-series-11"
+  slug: "apple-watch-series-11"
  productSlug: "apple-watch-series-11"
   brand: "Apple",
   category: "accessories",
