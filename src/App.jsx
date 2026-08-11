a46cd66eb3e7098fea735f4984ea90c0c50c7e3d
toji-t <mahadmk1@gmail.com>
2026-08-11 11:51:57 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import ReviewDetail from "./pages/ReviewDetail";
 import BuyingGuides from "./pages/BuyingGuides";
 import Blog from "./pages/Blog";
 import AISkills2026 from "./pages/AISkills2026";
+import StudentDeskSetup2026 from "./pages/StudentDeskSetup2026";
 import GuideDetail from "./pages/GuideDetail";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
@@ -28,8 +29,13 @@ export default function App() {
         <Route path="guides" element={<BuyingGuides />} />
         <Route path="guides/:slug" element={<GuideDetail />} />
         <Route path="blog" element={<Blog />} />
-        <Route path="blog/ai-skills-2026" element={<AISkills2026 />} />
-        <Route path="about" element={<About />} />
+<Route path="blog/ai-skills-2026" element={<AISkills2026 />} />
+<Route
+  path="blog/student-desk-setup-2026"
+  element={<StudentDeskSetup2026 />}
+/>
+<Route path="about" element={<About />} />
+        
         <Route path="contact" element={<Contact />} />
         <Route path="privacy-policy" element={<Privacy />} />
         <Route path="affiliate-disclosure" element={<AffiliateDisclosure />} />
