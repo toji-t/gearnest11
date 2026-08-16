1af4c560379a424c5978ada497e0c6a4082f3918
toji-t <mahadmk1@gmail.com>
2026-08-16 13:44:48 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -64,9 +64,9 @@ export default function App() {
         <Route
   path="blog/fast-charging-myths"
   element={<FastChargingMyths />}
-/>
+/> 
  
-<Route path="about" element={<About />} />
+<Route path="about" element={<About />} 
         
         <Route path="contact" element={<Contact />} />
         <Route path="privacy-policy" element={<Privacy />} />
